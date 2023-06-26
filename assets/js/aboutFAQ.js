70a06c73e538892e734aea552164266034e5ec1f
Robin <robin.t@live.be>
2023-06-26 15:15:51 +0200

    commit modif faq in about

--- assets/js/aboutFAQ.js
+++ assets/js/aboutFAQ.js
@@ -8,19 +8,29 @@ questions.forEach(question => {
     const answer = question.nextElementSibling;
 
     // Vérifier si la réponse est déjà ouverte ou fermée
-    const isOpen = answer.style.display === 'block';
-
+    //const isOpen = answer.style.display === 'block';
+    
     // Fermer toutes les autres réponses
-    questions.forEach(q => {
-      const a = q.nextElementSibling;
-      q.classList.remove('active');
-      a.style.display = 'none';
-    });
-
+    
+    /*
     if (!isOpen) {
       // Ouvrir la réponse
       question.classList.add('active');
       answer.style.display = 'block';
+    }*/
+    
+    if (answer.classList.contains('height')) {
+      question.querySelector("div").classList.remove('clicked')
+      answer.classList.remove('height');
+    } else {
+      questions.forEach(q => {
+        const a = q.nextElementSibling;
+        a.classList.remove('height');
+        q.querySelector("div").classList.remove('clicked')
+      });
+      
+      question.querySelector("div").classList.add('clicked')
+      answer.classList.add('height');
     }
   });
 });
